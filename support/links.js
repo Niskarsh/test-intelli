cf423dcce03e47738c14b3ba935fddf2a077d64e
Niskarsh Kumar <niskarshk@gmail.com>
2018-08-06 21:58:04 +0530

    Links done

--- support/links.js
+++ support/links.js
@@ -3,14 +3,15 @@ import cheerio from 'cheerio'
 
 export const getLinks = async () => {
 
-    let link = []
+    let links = []
 
     await request.get("https://10times.com/nationalfarm-machinery-show/exhibitors").then(html => {
         let $ = cheerio.load(html)
         let tableRows = $('div#block-listing.box').children("table.tb-list").children("tbody").children()
         tableRows.map((i, row) => {
-            link[i] = tableRows.eq(i).children().eq(1).children().next().children().attr('href')
+            links[i] = tableRows.eq(i).children().eq(1).children().next().children().attr('href')
         })
     }).catch(e => console.log(`Server down, couldn't ping. Error ${e}`))
-    return (link)
+    
+    return (links)
 }
